7e8a76417b4a6a6996469535491291908e8d81d1
Adonis Gaitatzis <backupbrain@gmail.com>
2016-11-02 15:10:16 -0700

    Fixed context holding issues

--- 10. Flow Control/app/src/main/java/tonyg/example/com/exampleblescan/views/BleGattProfileListAdapter.java
+++ 10. Flow Control/app/src/main/java/tonyg/example/com/exampleblescan/views/BleGattProfileListAdapter.java
@@ -16,7 +16,8 @@ import tonyg.example.com.exampleblescan.ble.BleDevice;
 
 
 /**
- * Manages the BleGattServiceListItem so that we can populate the list
+ * Manages the BleGattServiceListItem so that we can populate the GATT Profile List
+ * Uses a BaseExpandableListAdapter to create a tree-like structure
  *
  * @author Tony Gaitatzis backupbrain@gmail.com
  * @date 2015-12-21
@@ -36,33 +37,54 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
     }
     */
 
+    /**
+     * Instantiate the class
+     */
     public BleGattProfileListAdapter() {
 
     }
 
+    /** PARENT (SERVICE) METHODS FOR COLLAPSIBLE TREE STRUCTURE **/
 
-    /** Parent (Service) methods **/
+    /**
+     * Get the Service ListItem listed in a groupPosition
+     * @param groupPosition the position of the ListItem
+     * @return the ServiceListItem
+     */
     public BleGattServiceListItem getGroup(int groupPosition) {
         return mBleGattServiceListItems.get(groupPosition);
     }
 
+    /**
+     * How many Services are listed
+     * @return number of Services
+     */
     public int getGroupCount() {
         return mBleGattServiceListItems.size();
     }
 
-
+    /**
+     * Add a new Service to be listed in the ListView
+     * @param service the GATT Service to add
+     */
     public void addService(BluetoothGattService service) {
         int serviceItemID = mBleGattServiceListItems.size()-1;
         BleGattServiceListItem serviceListItem = new BleGattServiceListItem(service, serviceItemID);
         mBleGattServiceListItems.add(serviceListItem);
         mBleCharacteristicListItems.put(serviceItemID, new ArrayList<BleGattCharacteristicListItem>());
 
+        // update the UI
         notifyDataSetChanged();
     }
 
-
+    /**
+     * Add a new Characteristic to be listed in the ListView
+     * @param service the Service that this Characteristic belongs to
+     * @param characteristic the Gatt Characteristic to add
+     * @throws Exception if such a service does not exist
+     */
     public void addCharacteristic(BluetoothGattService service, BluetoothGattCharacteristic characteristic) throws Exception {
-        // find the service with this UUID
+        // find the Service in this listView with matching UUID from the input service
         int serviceItemId = -1;
         for (BleGattServiceListItem bleGattServiceListItem : mBleGattServiceListItems) {
             if (bleGattServiceListItem.getService().equals(service)) {
@@ -70,28 +92,37 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
             }
         }
 
+        // Throw an exception if no such service exists
         if (serviceItemId < 0) throw new Exception("Service not found with UUID: "+service.getUuid().toString());
 
+        // add characterstic to the end of the sub-list for the parent service
         int characteristicItemId = mBleCharacteristicListItems.size();
-
         BleGattCharacteristicListItem characteristicListItem = new BleGattCharacteristicListItem(characteristic, characteristicItemId);
-
         mBleCharacteristicListItems.get(serviceItemId).add(characteristicListItem);
 
         notifyDataSetChanged();
     }
 
+    /**
+     * Clear all ListItems from ListView
+     */
     public void clear() {
         mBleGattServiceListItems.clear();
         mBleCharacteristicListItems.clear();
     }
 
+    /**
+     * Get the Service ListItem at some position
+     *
+     * @param position the position of the ListItem
+     * @return BleGattServiceListItem at position
+     */
     public long getGroupId(int position) {
         return mBleGattServiceListItems.get(position).getItemId();
     }
 
     /**
-     * What UI stuff is contained in this List Item
+     * This GroupViewHolder represents what UI components are in each Service ListItem in the ListView
      */
     public static class GroupViewHolder{
         public TextView mUuid;
@@ -99,12 +130,22 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
     }
 
 
+    /**
+     * Generate a new Service ListItem for some known position in the ListView
+     *
+     * @param position the position of the Service ListItem
+     * @param convertView An existing List Item
+     * @param parent The Parent ViewGroup
+     * @return The Service List Item
+     */
     public View getGroupView(int position, boolean isExpanded, View convertView, ViewGroup parent) {
         View v = convertView;
         GroupViewHolder holder;
 
+        // if this ListItem does not exist yet, generate it
+        // otherwise, use it
         if(convertView == null) {
-            // convert list_item_device.xmlice.xml to a View
+            // convert list_item_device.xml to a View
             LayoutInflater inflater = LayoutInflater.from(parent.getContext());
             v = inflater.inflate(R.layout.list_item_ble_service, parent, false);
 
@@ -119,6 +160,8 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
             holder = (GroupViewHolder) v.getTag();
         }
 
+        // if there are known Services, create a ListItem that says so
+        // otherwise, display a ListItem with Bluetooth Service information
         if (getGroupCount() <= 0) {
             holder.mUuid.setText(R.string.no_data);
         } else {
@@ -126,6 +169,7 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
 
             holder.mUuid.setText(item.getUUIDString());
             int type = item.getType();
+            // Is this a primary or secondary service
             if (type == BluetoothGattService.SERVICE_TYPE_PRIMARY) {
                 holder.mType.setText(R.string.service_type_primary);
             } else {
@@ -135,59 +179,101 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
         return v;
     }
 
-
+    /**
+     * The IDs for this ListView do not change
+     * @return <b>true</b>
+     */
     public boolean hasStableIds() {
         return true;
     }
 
 
 
-    /** Child methods **/
+    /** CHILD (CHARACTERISTIC) METHODS FOR COLLAPSIBLE TREE STRUCTURE **/
 
+    /**
+     * Get the Characteristic ListItem at some position in the ListView
+     * @param groupPosition the position of the Service ListItem in the ListView
+     * @param childPosition the sub-position of the Characteristic ListItem under the Service
+     * @return BleGattCharactersiticListItem at some groupPosition, childPosition
+     */
     public BleGattCharacteristicListItem getChild(int groupPosition, int childPosition) {
         return mBleCharacteristicListItems.get(groupPosition).get(childPosition);
     }
 
+    /**
+     * Get the ID of a Charactersitic ListItem
+     *
+     * @param groupPosition the position of a Service ListItem
+     * @param childPosition The sub-position of a Characteristic ListItem
+     * @return the ID of the Characteristic ListItem
+     */
     public long getChildId(int groupPosition, int childPosition) {
         return childPosition;
     }
 
 
+    /**
+     * How many Characteristics exist under a Service
+     *
+     * @param groupPosition The position of the Service ListItem in the ListView
+     * @return the number of Characteristics in this Service
+     */
     public int getChildrenCount(int groupPosition) {
         return mBleCharacteristicListItems.get(groupPosition).size();
     }
 
+    /**
+     * Charactersitics are selectable because the user can click on them to open the TalkActivity
+     *
+     * @param groupPosition The Service ListItem position
+     * @param childPosition The Charecteristic ListItem sub-position
+     * @return <b>true</b>
+     */
     public boolean isChildSelectable(int groupPosition, int childPosition) {
         return true;
     }
 
 
 
+    /**
+     * This ChildViewHolder represents what UI components are in each Characteristic List Item in the ListView
+     */
     public static class ChildViewHolder{
         public TextView mUuid;
     }
 
+    /**
+     * Generate a new Characteristic ListItem for some known position in the ListView
+     *
+     * @param groupPosition the position of the Service ListItem
+     * @param childPosition the position of the Characterstic ListItem
+     * @param convertView An existing List Item
+     * @param parent The Parent ViewGroup
+     * @return The Characteristic ListItem
+     */
     public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         BleGattCharacteristicListItem item = getChild(groupPosition, childPosition);
         BluetoothGattCharacteristic characteristic = item.getCharacteristic();
 
+        // if this ListItem does not exist yet, generate it
+        // otherwise, use it
         if (convertView == null) {
-
             LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-
             convertView = inflater.inflate(R.layout.list_item_ble_characteristic, null);
         }
 
+        // prep the View
         TextView uuidTV = (TextView) convertView.findViewById(R.id.uuid);
-
-
         TextView mPropertyReadable = (TextView)convertView.findViewById(R.id.property_read);
         TextView mPropertyWritable = (TextView)convertView.findViewById(R.id.property_write);
         TextView mPropertyNotifiable = (TextView)convertView.findViewById(R.id.property_notify);
         TextView mPropertyNone = (TextView)convertView.findViewById(R.id.property_none);
 
+        // display the UUID of the characteristic
         uuidTV.setText(characteristic.getUuid().toString());
 
+        // Display the read/write/notify attributes of the Characteristic
         if (BleDevice.isCharacteristicReadable(characteristic)) {
             mPropertyReadable.setVisibility(View.VISIBLE);
         } else {
@@ -203,7 +289,6 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
         } else {
             mPropertyNotifiable.setVisibility(View.GONE);
         }
-
         if (!BleDevice.isCharacteristicNotifiable(characteristic) &&
                 !BleDevice.isCharacteristicWritable(characteristic) &&
                 !BleDevice.isCharacteristicReadable(characteristic)) {
@@ -212,6 +297,8 @@ public class BleGattProfileListAdapter extends BaseExpandableListAdapter {
             mPropertyNone.setVisibility(View.GONE);
 
         }
+
+
         return convertView;
     }
 
